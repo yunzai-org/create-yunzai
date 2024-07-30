55a898d2a34edb092cbf63eda22b7c02067a967f
xiejihuang@baidu.com <xiejihuang@baidu.com>
2024-07-30 16:22:18 +0800

    feat: 修改模板

--- bin/template/src/apps/disFriPoke.ts
+++
@@ -1,8 +0,0 @@
-import { ConfigController as cfg, Messages } from 'yunzai'
-const message = new Messages('notice.friend.poke')
-message.use(e => {
-  if (!cfg.other?.disablePrivate) return
-  if (e.isMaster) return
-  e.reply(cfg.other.disableMsg)
-})
-export const disFriPoke = message.ok
